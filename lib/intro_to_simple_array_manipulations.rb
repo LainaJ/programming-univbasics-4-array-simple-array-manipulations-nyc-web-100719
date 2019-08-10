1cb826094bf11e3886f23f69aaecbc46cd8107aa
Laina Karosic <lainakarosic@gmail.com>
2019-08-10 21:33:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat
-  array.concat
+def using_concat(element1, element2)
+  array.concat(element1, element2)
 end
 
 def using_insert
